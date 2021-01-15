ef0a9a97df6469ac3b6f4b6b7399a5e2f8d51b6a
daddabarba <davidebarbieri97@gmail.com>
2021-01-15 18:03:55 +0100

    Made online counting

--- count.py
+++ count.py
@@ -6,56 +6,32 @@ import sys
 
 ######## NOT FINISHED (do not touch for now)#####################
 
-def feature_extractor(c, feat, df):
+def feature_extractor(sentence, feat):
 
     l=[]
-    feature = df[df['sense'] == c]
 
-    #print(feature[feat].tolist())
 
-    for item in feature.loc[:, feat].values:
-
-        for feat_name, feat_vals in zip(feat, item):
-
-            cont_elem= list(feat_vals.strip("] [").replace("'", '').split(", "))
-
-            for i in cont_elem:
-
-                l.append(feat_name + '_' + i)
+    for _, word_data in sentence.iterrows():
+        for feat_name in feat:
+            l.append(feat_name + '_' + word_data[feat_name])
 
     return l
 
-def counter(classes, feature, df):
-    counter_f= {}
+def counter(c, features, counter_f = {}):
 
-    for c in classes:
-
+    if c not in counter_f.keys():
         counter_f[c]={}
 
-        out = feature_extractor(c, feature, df)
+    for f in features:
 
-        for f in out:
+        if f not in counter_f[c].keys():
+            counter_f[c][f]= 0
 
-            if f not in counter_f[c].keys():
-                counter_f[c][f]= 0
-
-            counter_f[c][f]+=1
+        counter_f[c][f]+=1
 
 
     return counter_f
 
 #########################################################
 
-if __name__ == '__main__':
 
-    df = pd.read_csv (r'context_size_3.csv')
-
-    print(df)
-    
-    c=["male.n.02","carry.v.01"]
-    gen_feat=["sym_context","sem_context","sns_context"]
-
-
-    #out=feature_extractor(c, gen_feat[0], df)
-    count= counter(c,gen_feat, df)
-    print(count)
---
+++ online_window.py
@@ -0,0 +1,87 @@
+from window_slider import Slider
+from IPython.display import display
+import numpy as np
+import pandas as pd
+import sys
+
+from count import *
+
+
+def pad_both(lst, pad_size):
+    before = pad_size * ["x"]
+    after = (pad_size - 1) * ["x"]
+    lst = before + lst + after
+    # print(lst)
+    return lst
+
+
+def pad(lst, n):
+    cnt = 0
+    while cnt < n:
+        lst = np.insert(lst, 0, "x")
+        cnt += 1
+    # ~ print(lst)
+    return list(lst)
+
+
+def extract_dataset(file_name="dev.csv"):
+    return pd.read_csv(file_name, index_col=0)
+
+def scrolling_window(dataset_name, bucket_size, features):
+
+    df = extract_dataset(dataset_name)
+
+    counter_f = {}
+
+    for sent_id in df['sentence'].unique():
+
+        # get dataset for sentence and list the words in it
+        sentence = df[df['sentence'] == sent_id]
+        words = list(sentence['word'].values)        
+        words_idx = list(sentence.index)
+
+        # pad with "x"
+        words = np.array(pad_both(words, int((bucket_size - 1) / 2)))
+        words_idx = np.array(pad_both(words_idx, int((bucket_size - 1) / 2)))
+
+        if len(words) >= bucket_size:
+
+            overlap_count = bucket_size - 1
+            slider_words = Slider(bucket_size, overlap_count)
+            slider_words.fit(words)
+
+            slider_idx = Slider(bucket_size, overlap_count)
+            slider_idx.fit(words_idx)
+
+            while True:
+
+                window_words = slider_words.slide()
+                window_idx = list(map(lambda x : int(x), filter(lambda x : x!='x', slider_idx.slide())))
+                window_data = sentence.loc[window_idx, :]
+
+
+                # TODO match with sentences with padding on both sides
+                main_word = list(window_words)[int(overlap_count / 2)]
+                if main_word == "x": break
+                sense = sentence[sentence['word'] == main_word]['sns'].values[0]
+
+                # only consider words for "main word" when they have a sense
+                if sense != 'O':
+                    
+                    extracted_features = feature_extractor(window_data, features)
+                    counter_f = counter(sense, extracted_features, counter_f)
+
+                if slider_words.reached_end_of_list(): break
+
+    # display(out)
+    # out.to_csv(f"context_size_{bucket_size}.csv")
+
+    return counter_f
+
+
+if __name__ == '__main__':
+
+
+    
+    gen_feat=["sym"]
+    count= scrolling_window("dev.csv", 3, gen_feat)
